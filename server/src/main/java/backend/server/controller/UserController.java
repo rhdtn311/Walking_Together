495854ffbf0aa400ba8453be0b93c7a52dad78ad
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-10-05 12:45:36 +0900

    리팩토링 : UserService 삭제, UserController import 정리

--- server/src/main/java/backend/server/controller/UserController.java
+++ server/src/main/java/backend/server/controller/UserController.java
@@ -1,24 +1,13 @@
 package backend.server.controller;
 
-import backend.server.DTO.FindPasswordDTO;
 import backend.server.DTO.user.UserDTO;
 import backend.server.DTO.response.ResponseDTO;
-import backend.server.entity.Member;
-import backend.server.exception.ApiException;
-import backend.server.message.Message;
 import backend.server.service.user.PasswordFineService;
 import backend.server.service.user.SignUpService;
-import backend.server.service.user.UserService;
 import backend.server.service.user.VerificationNumberSendService;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static java.lang.Math.abs;
 
@@ -26,7 +15,6 @@ import static java.lang.Math.abs;
 @RestController
 public class UserController {
 
-    private final UserService userService;
     private final SignUpService signUpService;
     private final VerificationNumberSendService verificationNumberSendService;
     private final PasswordFineService passwordFineService;
@@ -43,7 +31,7 @@ public class UserController {
     }
 
     // 회원가입시 인증코드 전송
-    @GetMapping("/signup/authNum") // {}로 바꾸기
+    @GetMapping("/signup/authNum")
     public ResponseEntity<ResponseDTO> sendVerificationNumber(@RequestParam(value = "email", required = false) String email) {
         UserDTO.VerificationNumberSendResDTO verificationNumberSendResDTO
                 = verificationNumberSendService.sendVerificationNumber(email);
@@ -52,19 +40,6 @@ public class UserController {
                     .message("메일이 성공적으로 발송되었습니다.")
                     .data(verificationNumberSendResDTO)
                     .build());
-    }
-
-    @GetMapping("/user")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<Member> getMyUserInfo() {
-
-        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
-    }
-
-    @GetMapping("/user/{username}")
-    @PreAuthorize("hasAnyRole('ADMIN')")
-    public ResponseEntity<Member> getUserInfo(@PathVariable String username) {
-        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
     }
 
     // 비밀번호 찾기
@@ -76,11 +51,5 @@ public class UserController {
                 .message("임시 비밀번호가 입력하신 이메일로 발송 되었습니다.")
                 .data(passwordFindResDTO)
                 .build());
-    }
-
-    @GetMapping("/authenticTest")
-    public String authenticTest(@RequestBody String str) {
-
-        return str;
     }
 }
