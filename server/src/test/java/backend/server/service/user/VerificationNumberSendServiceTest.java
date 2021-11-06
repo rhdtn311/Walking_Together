e6ae6ab5b9d5657d04b27358fce2e3029b7c7e10
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2021-11-06 13:49:19 +0900

    테스트 코드 작성 : VerificationNumberSendServiceTest
    인증 코드 확인, 비즈니스 예외 확인

---
+++ server/src/test/java/backend/server/service/user/VerificationNumberSendServiceTest.java
@@ -0,0 +1,64 @@
+package backend.server.service.user;
+
+import backend.server.DTO.user.UserDTO;
+import backend.server.entity.Member;
+import backend.server.exception.userService.EmailDuplicationException;
+import backend.server.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class VerificationNumberSendServiceTest {
+
+    @Autowired
+    VerificationNumberSendService verificationNumberSendService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("회원가입 시 인증 번호가 바뀌는지, 7자리가 맞는지 확인")
+    void verificationNumber() {
+        // given
+        UserDTO.VerificationNumberSendResDTO dto =
+                verificationNumberSendService.sendVerificationNumber("email1@naver.com");
+
+        // when
+        UserDTO.VerificationNumberSendResDTO dto2
+                = verificationNumberSendService.sendVerificationNumber("email2@naver.com");
+
+        // then
+        assertThat(dto.getAuthNum()).isNotEqualTo(dto2.getAuthNum());
+        assertThat(dto.getAuthNum().length()).isEqualTo(dto2.getAuthNum().length()).isEqualTo(7);
+    }
+
+    @Test
+    @DisplayName("해당 이메일이 이미 가입돼있을 때 비즈니스 예외")
+    void existsEmail() {
+        // given
+        Member member = Member.builder()
+                .email("email1@naver.com")
+                .name("member")
+                .department("department")
+                .phoneNumber("010-0000-0000")
+                .birth("1996-03-11")
+                .password("password")
+                .stdId("stdId")
+                .build();
+
+        // when
+        userRepository.save(member);
+
+        // then
+        assertThrows(EmailDuplicationException.class,
+                () -> verificationNumberSendService.sendVerificationNumber(member.getEmail()));
+    }
+}
