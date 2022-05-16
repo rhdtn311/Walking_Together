7762e4d8217e2f94c4c08b932f2c7e06453024fe
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-05-16 19:35:52 +0900

    테스트 코드 작성 : ActivityControllerTest

---
+++ server/src/test/java/backend/server/controller/ActivityControllerTest.java
@@ -0,0 +1,13 @@
+package backend.server.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ActivityControllerTest {
+
+
+
+}
