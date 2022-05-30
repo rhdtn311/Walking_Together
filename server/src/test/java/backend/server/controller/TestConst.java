19f6ad71c271bf86f56e2aacbf691235eefcd0a4
rhdtn311 <68289543+rhdtn311@users.noreply.github.com>
2022-05-30 15:53:38 +0900

    TestConst 추가 - MockMultipartFile

--- server/src/test/java/backend/server/controller/TestConst.java
+++ server/src/test/java/backend/server/controller/TestConst.java
@@ -1,5 +1,10 @@
 package backend.server.controller;
 
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -18,5 +23,16 @@ public class TestConst {
     static String timeToString(LocalTime time) {
 
         return "[" + time.getHour() + "," + time.getMinute() + "," + time.getSecond() + "," + time.getNano() + "]";
+    }
+
+
+    static MockMultipartFile getMockMultipartFile() throws IOException {
+
+        String fileName = "name";
+        String contentType = "png";
+        String filePath = "src/test/java/imageFiles/ActivityStartImage.png";
+
+        FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+        return new MockMultipartFile(fileName, fileName + "." + contentType, contentType, fileInputStream);
     }
 }
